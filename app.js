19ba21e4a274a7722e27846e00926aa5338f485e
Ananda Prem R <ap7leo@gmail.com>
2025-10-11 03:13:57 +0530

    creatred env files

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Ho, World!");
+import express from "express";
+import { PORT } from "./env.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to your first ever handwritten backend get req");
+});
+
+app.listen(PORT, () => {
+  console.log("Subscription Tracker API is running on PORT 3000  ");
+});
+
+export default app;
