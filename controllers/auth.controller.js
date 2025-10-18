39fcb3fa82ea517e5642c0b68d3f07ee12cc25b1
Ananda Prem R <ap7leo@gmail.com>
2025-10-19 01:05:00 +0530

    sign-in api is officially

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -51,7 +51,42 @@ export const signUp = async (req,res,next) => {
 }
 
 
-export const signIn = async (req,res,next) => {}
+export const signIn = async (req,res,next) => {
+    try{
+        const {email,password}=req.body;
+        const signInUser= await User.findOne({email})
+   
+       
+       if(!signInUser){
+        const error = new Error('User Not Found');
+        error.statusCode=404;
+        throw error;
+       }
+
+       const isPasswordValid= await bcrypt.compare(password,signInUser.password)
+
+       if(!isPasswordValid){
+        const error = new Error('Invalid Password!');
+        error.statusCode=401;
+        throw error;
+          
+       }
+
+       const token = jwt.sign({userId:signInUser._id},JWT_SECRET,{expiresIn:JWT_EXPIRES_IN}) 
+       
+       res.status(200).json({
+        success:true,
+        message:"User signed in successfully",
+        data:{
+            token,
+            signInUser,
+        }
+       })
+    }catch(error){
+        next(error)
+       }
+
+}
 
 export const signOut = async (req,res,next) => {}
 
