35df37290562eb0e7b2fb5e6e4a403b687126f3f
Ananda Prem R <ap7leo@gmail.com>
2025-10-19 03:21:55 +0530

    added the user controller and made the get user and get user by id workin the user routes page via the user controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import User from '../models/user.model.js'
+ 
 import bcrypt from 'bcryptjs'
 import jwt from "jsonwebtoken"
 import {JWT_SECRET,JWT_EXPIRES_IN} from '../env.js'
