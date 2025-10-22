4198566f953df2822adf5caa3f370aa41b556424
Ananda Prem R <ap7leo@gmail.com>
2025-10-23 00:58:16 +0530

    so for the rate limiting i added that each request deducts one token from the token bucket

--- middlewares/arjcjet.middleware.js
+++ middlewares/arjcjet.middleware.js
@@ -2,9 +2,24 @@ import aj from '../config/arcjet.js'
 
 const arjectMiddleware =   async (req,res,next)=>{
     try{
-        const decision = await aj.protect(req);
+        const decision = await aj.protect(req,{requested:1}); //this requested :1 means that each request will deduct 1 token 
+
+        if(decision.isDenied()){
+            if(decision.reason.isRateLimit()){
+                return res.status(429).json({error:'Rate Limit Exceeded!'})
+            }
+        
+
+        if(decision.reason.isBot()){
+                return res.status(403).json({error:'Bot Exceeded'})
+            }
+        
+    }
+
+    next();
     }
     catch(error){
         console.log(`ArcJect MiddleWare error:${error}`);
+        next(error);
     }
 }
