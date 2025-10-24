9129541f9114cf25c5935a9114b693ddbe6980f0
Ananda Prem R <ap7leo@gmail.com>
2025-10-24 23:12:37 +0530

    added some routes to the subscriptions routes page for the feautures for our subcription tracker

--- ROUTES/subscriptions.routes.js
+++ ROUTES/subscriptions.routes.js
@@ -1,10 +1,27 @@
+
 import { Router } from 'express';
-import  {createSubscription} from '../controllers/subscription.controller.js'
-import authorize from '../middlewares/auth.middleware.js'
+import { createSubscription, getUserSubscriptions } from '../controllers/subscription.controller.js';
+import authorize from '../middlewares/auth.middleware.js';
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.post('/',authorize,createSubscription);      
+subscriptionRouter.get('/', (req, res) => res.send({ title: 'GET all subscriptions' }));
+
+subscriptionRouter.get('/:id', (req, res) => res.send({ title: 'GET subscription details' }));
+
+subscriptionRouter.post('/', authorize, createSubscription);
+
+
+subscriptionRouter.put('/:id/cancel', authorize, (req, res) => res.send({ title: 'CANCEL subscription' }));
+
+subscriptionRouter.get('/upcoming-renewals', authorize, (req, res) => res.send({ title: 'GET upcoming renewals' }));
+
+subscriptionRouter.put('/:id', authorize, (req, res) => res.send({ title: 'UPDATE subscription' }));
+
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
+
+
+subscriptionRouter.delete('/:id', authorize, (req, res) => res.send({ title: 'DELETE subscription' }));
+
 
 export default subscriptionRouter;
-
